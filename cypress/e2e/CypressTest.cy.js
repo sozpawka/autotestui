2432cb60668d980679f14a55116c10e3200d334f
Sozpawka <sozpawkawar@gmail.com>
2026-05-14 11:06:52 +0700

    9_fix_bugs

--- cypress/e2e/CypressTest.cy.js
+++ cypress/e2e/CypressTest.cy.js
@@ -100,3 +100,94 @@ describe('Cypress Tests', () => {
     });
   });
 });
+
+describe('Cypress Negative Tests', () => {
+
+  it('8. Негативный тест: Создание стажировки с пустым названием', () => {
+    cy.fixture('cypressTest').then((data) => {
+      cy.visit(data.main_url + '/login');
+      cy.get('.form-input--text').type(data.login);
+      cy.get('.form-input--password').type(data.password);
+      cy.get(':nth-child(3) > .button').click();
+      
+      cy.get('.menu-item__item-name').contains('Стажировки').click();
+      cy.get('.vacancies-block__filters-wrapper > .button').first().should('exist');
+      cy.get('.vacancies-block__filters-wrapper > .button').first().click({ force: true });
+      
+      cy.get('.desktop-modal__content').should('be.visible');
+      cy.get('.vacancy-add-form-wrapper > .form > .form__buttons > .buttons > .button').click({ force: true });
+      cy.get('.desktop-modal__content').should('be.visible');
+    });
+  });
+
+  it('9. Негативный тест: Поиск несуществующей стажировки', () => {
+    cy.intercept('GET', '**/api/internships?page=1*').as('getSearch');
+    cy.fixture('cypressTest').then((data) => {
+      cy.visit(data.main_url + '/internships');
+      cy.get('.form-input--text').first().type('ASDFG12345');
+      cy.get('div.search-input__field > .button').click();
+            cy.wait('@getSearch');
+      cy.wait(500); 
+      cy.get('body').should('not.contain', 'Тестовая стажировка');
+    });
+  });
+
+  it('10. Негативный тест: Отклик на уже откликнутую стажировку', () => {
+    cy.fixture('cypressTest').then((data) => {
+      cy.visit(data.main_url + '/login');
+      cy.get('.form-input--text').type('Sozpawka');
+      cy.get('.form-input--password').type(data.password);
+      cy.get(':nth-child(3) > .button').click();
+      
+      cy.url().should('include', '/account');
+      cy.wait(2000); 
+
+      cy.visit(data.main_url + '/internships');
+      
+      cy.get('.internship-item__footer-wrapper').should('be.visible');
+
+      cy.contains('Вы уже откликнулись!')
+        .first()
+        .parents('.internship-item__footer-wrapper')
+        .within(() => {
+          cy.get('button').first().click({ force: true });
+          cy.get('.button__background-color-green').should('not.exist');
+        });
+    });
+  });
+
+  it('11. Негативный тест: подтверждения в отклоненных', () => {
+    cy.fixture('cypressTest').then((data) => {
+      cy.visit(data.main_url + '/login');
+      cy.get('.form-input--text').type(data.login);
+      cy.get('.form-input--password').type(data.password);
+      cy.get(':nth-child(3) > .button').click();
+      
+      cy.get(':nth-child(5) > .menu-item__item-name').click();
+      cy.wait(1000); 
+
+      cy.get(':nth-child(4) > .navigation-item__title').should('be.visible').click(); 
+      cy.wait(1000);
+
+      cy.get('.responses-list').should('not.contain', 'Вы подтвердили заявку');
+    });
+  });
+
+  it('12. Негативный тест: Отправка пустого комментария', () => {
+    cy.fixture('cypressTest').then((data) => {
+      cy.visit(data.main_url + '/login');
+      cy.get('.form-input--text').type(data.login);
+      cy.get('.form-input--password').type(data.password);
+      cy.get(':nth-child(3) > .button').click();
+      
+      cy.get(':nth-child(5) > .menu-item__item-name').click();
+      cy.get(':nth-child(3) > .navigation-item__title').click();
+      cy.get('.infinite-loader > :nth-child(1) > .button').click();
+      
+      cy.get('.comment-textarea__buttons > :nth-child(2)').click({ force: true });
+      
+      cy.get('.base-comment').should('not.have.text', '');
+    });
+  });
+
+});
